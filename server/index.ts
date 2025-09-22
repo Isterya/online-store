9645931a3c3c8a6490959871de86e01d4915739b
isterya <vash.vybor.1@gmail.com>
2025-09-22 19:34:21 +0200

    feat: add cors to the server

--- server/index.ts
+++ server/index.ts
@@ -2,13 +2,16 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 import express from 'express';
-import sequelize from './db';
+import cors from 'cors';
 
+import sequelize from './db';
 import './models/models';
 
 const PORT = process.env.PORT || 5000;
 
 const app = express();
+app.use(cors());
+app.use(express.json());
 
 const start = async () => {
   try {
