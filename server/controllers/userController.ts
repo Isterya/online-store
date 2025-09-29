16c353630079cb9186bf630145ec296e9a00a96e
isterya <vash.vybor.1@gmail.com>
2025-09-29 18:50:43 +0200

    feat: implement the registration system in UserController

--- server/controllers/userController.ts
+++ server/controllers/userController.ts
@@ -1,8 +1,43 @@
 import { Request, Response, NextFunction } from 'express';
 import ApiError from '../error/ApiError';
 
+import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
+
+import { models } from '../models/models';
+const { User, Basket } = models;
+
+const generateJwt = (id: number, email: string, role: string) => {
+  return jwt.sign({ id: id, email, role }, process.env.SECRET_KEY as string, {
+    expiresIn: '24h',
+  });
+};
+
 class UserController {
-  async registration(req: Request, res: Response) {}
+  async registration(req: Request, res: Response, next: NextFunction) {
+    const { email, password, role } = req.body;
+
+    if (!email || !password) {
+      return next(ApiError.badRequest('Invalid email or password'));
+    }
+
+    const candidate = await User.findOne({ where: { email } });
+    if (candidate) {
+      return next(ApiError.badRequest('User with this email already exists'));
+    }
+
+    const hashPassword = await bcrypt.hash(password, 5);
+    const user = (await User.create({
+      email,
+      role,
+      password: hashPassword,
+      // FIX: Type casting
+    })) as any;
+    const basket = await Basket.create({ userId: user.id });
+
+    const token = generateJwt(user.id, user.email, user.role);
+    return res.json({ token });
+  }
 
   async login(req: Request, res: Response) {}
 
--- server/routes/index.ts
+++ server/routes/index.ts
@@ -7,7 +7,7 @@ import typeRouter from './typeRouter';
 import brandRouter from './brandRouter';
 import deviceRouter from './deviceRouter';
 
-router.use('/users', userRouter);
+router.use('/user', userRouter);
 router.use('/type', typeRouter);
 router.use('/brand', brandRouter);
 router.use('/device', deviceRouter);
