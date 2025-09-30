50300f8cb5a5816627cfa9704de02abf1c87d7b2
isterya <vash.vybor.1@gmail.com>
2025-09-30 06:49:06 +0200

    feat: add checkRoleMiddleware

--- server/routes/typeRouter.ts
+++ server/routes/typeRouter.ts
@@ -3,7 +3,9 @@ import typeController from '../controllers/typeController';
 
 const router = Router();
 
-router.post('/', typeController.create);
+import checkRoleMiddleware from '../middleware/checkRoleMiddleware';
+
+router.post('/', checkRoleMiddleware('ADMIN'), typeController.create);
 router.get('/', typeController.getAll);
 
 export default router;
