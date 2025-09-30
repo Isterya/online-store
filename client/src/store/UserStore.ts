2a1ad72f02aac79721d30ee85e2e4625aaf43693
isterya <vash.vybor.1@gmail.com>
2025-09-30 08:31:37 +0200

    feat: add UserStore

---
+++ client/src/store/UserStore.ts
@@ -0,0 +1,32 @@
+import { makeAutoObservable } from 'mobx';
+
+interface IUser {
+  id?: number;
+  email?: string;
+  name?: string;
+}
+
+export default class UserStore {
+  private _isAuth = false;
+  private _user: IUser = {};
+
+  constructor() {
+    makeAutoObservable(this);
+  }
+
+  get isAuth() {
+    return this._isAuth;
+  }
+
+  get user() {
+    return this._user;
+  }
+
+  setIsAuth(bool: boolean) {
+    this._isAuth = bool;
+  }
+
+  setUser(user: IUser) {
+    this._user = user;
+  }
+}
