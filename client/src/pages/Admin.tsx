c9c19891a23d04d683562df0fe7810b45590d37e
isterya <vash.vybor.1@gmail.com>
2025-11-14 11:36:42 +0100

    feat: add buttons to the Admin page

--- client/src/pages/Admin.tsx
+++ client/src/pages/Admin.tsx
@@ -1,5 +1,13 @@
+import { Button, Container } from 'react-bootstrap';
+
 const Admin = () => {
-  return <div>ADMIN</div>;
+  return (
+    <Container className="d-flex flex-column">
+      <Button className="">Add type</Button>
+      <Button className="">Add brand</Button>
+      <Button className="">Add device</Button>
+    </Container>
+  );
 };
 
 export default Admin;
